3155f2a29ac0c22716dbe42e2545345c81e6e2c6
Muhammad Yahyo <san4o.original@gamil.com>
2022-06-11 23:53:22 +0500

    added app bar

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: Center(
+              child: Text(
+            'Accounts',
+          )),
+          leading: IconButton(
+            alignment: Alignment.centerLeft,
+            onPressed: () {},
+            icon: const Icon(Icons.settings),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+            ),
+          ],
         ),
         body: Center(
           child: Container(
