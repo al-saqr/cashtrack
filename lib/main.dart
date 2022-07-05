568a9e37ae52b534a2d2a872c5400f0c2bd0c7b2
Muhammad Yahyo <san4o.original@gamil.com>
2022-07-05 10:23:34 +0500

    verstile: main screen

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,8 @@ class MyApp extends StatelessWidget {
     'asdsad',
     'asdsad',
   ];
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
 
   @override
   Widget build(BuildContext context) {
@@ -64,7 +66,75 @@ class MyApp extends StatelessWidget {
             children: [
               const Center(child: Text('DAILY')),
               const Center(child: Text('WEEKLY')),
-              Container(child: CarouselWithIndicatorDemo()),
+              Container(
+                color: Colors.red.shade300,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      height: 100,
+                      child: Column(
+                        children: const [
+                          Text(
+                            'Balance',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white60,
+                            ),
+                          ),
+                          Text(
+                            '\$ 25 000 000',
+                            style: TextStyle(
+                              fontSize: 42,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            '% 5',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white60,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 6.0,
+                        horizontal: 12.0,
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8.0),
+                            color: Colors.white),
+                        child: ListTile(
+                          minVerticalPadding: 8.0,
+                          dense: true,
+                          title: const Text(
+                            'Credit card',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black45),
+                          ),
+                          subtitle: Text(
+                            '\$ 532',
+                            style: TextStyle(
+                              height: 2,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red.shade300,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
               const Center(child: Text('YEARLY')),
             ],
           ),
