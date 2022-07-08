db70725d4e455a33fd5934e841309fe53b822a2d
Muhammad Yahyo <san4o.original@gamil.com>
2022-07-09 03:30:17 +0500

    settings page in progress

--- lib/ui/pages/settings/settings_page.dart
+++ lib/ui/pages/settings/settings_page.dart
@@ -6,38 +6,93 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Settings',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: Colors.white,
-        leading: TextButton(
-          onPressed: () {},
-          child: const Text(
-            style: TextStyle(
-                color: Color.fromARGB(255, 55, 199, 175),
-                fontWeight: FontWeight.bold),
-            'Cancel',
-          ),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              'Save',
-              style: TextStyle(
-                color: Colors.green,
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: 45,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.white,
+                ),
+                Positioned(
+                  top: 13,
+                  right: 10,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Save',
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 13,
+                  left: 10,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Cancel',
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 13,
+                  left: MediaQuery.of(context).size.width / 2.5,
+                  child: const Text(
+                    'Settings',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.grey.shade300,
+              height: 45,
+              width: double.infinity,
+              child: Row(
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      left: 10,
+                      top: 20,
+                    ),
+                    child: Text(
+                      'Main',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.grey),
+                    ),
+                  )
+                ],
               ),
             ),
-          ),
-        ],
-      ),
-      body: Container(
-        color: Colors.transparent,
+            Container(
+              width: double.infinity,
+              color: Colors.blue,
+              child: Column(
+                children: [
+                  Row(
+                    children: [],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
