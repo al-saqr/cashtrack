97c1180efc9aefe45921882c53e7b25bdca492c2
Muhammad Yahyo <san4o.original@gamil.com>
2022-07-11 00:02:19 +0500

    protoyping stage complete, next stage designing and sizes fixing

--- lib/ui/pages/settings/settings_page.dart
+++ lib/ui/pages/settings/settings_page.dart
@@ -5,55 +5,292 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final heightOfDevice = MediaQuery.of(context).size.height;
+    final widthOfDevice = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Column(
+        children: const [
+          TopBarWidget(),
+          MainSettingsWidget(),
+          PremiumSettingsWidget(),
+          SubscribeButtonWidget(),
+          SendReviewButtonWidget(),
+          RateInAppStoreButtonWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class RateInAppStoreButtonWidget extends StatelessWidget {
+  const RateInAppStoreButtonWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.black12),
+        minimumSize: MaterialStateProperty.all(
+          const Size(300, 45),
+        ),
+      ),
+      child: Text('Rate in App Stpre'),
+    );
+  }
+}
+
+class SendReviewButtonWidget extends StatelessWidget {
+  const SendReviewButtonWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.black12),
+        minimumSize: MaterialStateProperty.all(
+          const Size(300, 45),
+        ),
+      ),
+      child: Text('Send your review'),
+    );
+  }
+}
+
+class SubscribeButtonWidget extends StatelessWidget {
+  const SubscribeButtonWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.green),
+        minimumSize: MaterialStateProperty.all(
+          const Size(300, 45),
+        ),
+      ),
+      child: Text('Subscribe'),
+    );
+  }
+}
+
+class PremiumSettingsWidget extends StatelessWidget {
+  const PremiumSettingsWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: 3,
+      child: Column(
         children: [
-          const TopBarWidget(),
           Flexible(
-            flex: 3,
-            child: Container(
-              child: Column(
+            child: TextButton(
+              onPressed: () {},
+              child: Row(
                 children: [
-                  Flexible(
-                    child: Row(
-                      children: [
-                        Expanded(
-                            flex: 1,
-                            child: Container(color: Colors.transparent)),
-                        Text('Currency'),
-                        Expanded(
-                          flex: 10,
-                          child: Container(
-                            color: Colors.transparent,
-                          ),
-                        ),
-                        Text('USD'),
-                        Icon(
-                          Icons.chevron_right,
-                          color: Colors.grey,
-                        ),
-                      ],
+                  Expanded(
+                      flex: 1, child: Container(color: Colors.transparent)),
+                  Text('Icon set'),
+                  Expanded(
+                    flex: 10,
+                    child: Container(
+                      color: Colors.transparent,
                     ),
                   ),
-                  Flexible(
+                  Icon(
+                    Icons.paid_outlined,
+                    color: Colors.green,
+                  ),
+                  Expanded(
+                    flex: 1,
                     child: Container(
-                      color: Colors.grey,
+                      color: Colors.transparent,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Flexible(
+            child: TextButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Expanded(
+                      flex: 1, child: Container(color: Colors.transparent)),
+                  Text('Backups'),
+                  Expanded(
+                    flex: 10,
+                    child: Container(
+                      color: Colors.transparent,
                     ),
                   ),
-                  Flexible(
+                  Icon(
+                    Icons.paid_outlined,
+                    color: Colors.green,
+                  ),
+                  Expanded(
+                    flex: 1,
                     child: Container(
-                      color: Colors.blue,
+                      color: Colors.transparent,
                     ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Flexible(
+            child: TextButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Expanded(
+                      flex: 1, child: Container(color: Colors.transparent)),
+                  Text('Export'),
+                  Expanded(
+                    flex: 10,
+                    child: Container(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  Icon(
+                    Icons.paid_outlined,
+                    color: Colors.green,
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.transparent,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Flexible(
+            child: TextButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Expanded(
+                      flex: 1, child: Container(color: Colors.transparent)),
+                  Text('Security'),
+                  Expanded(
+                    flex: 10,
+                    child: Container(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  Icon(
+                    Icons.paid_outlined,
+                    color: Colors.green,
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.transparent,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MainSettingsWidget extends StatelessWidget {
+  const MainSettingsWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: 3,
+      child: Column(
+        children: [
+          Flexible(
+            child: TextButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Expanded(
+                      flex: 1, child: Container(color: Colors.transparent)),
+                  Text('Currency'),
+                  Expanded(
+                    flex: 10,
+                    child: Container(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  Text('USD'),
+                  Icon(
+                    Icons.chevron_right,
+                    color: Colors.grey,
                   ),
                 ],
               ),
             ),
           ),
           Flexible(
-            child: Container(color: Colors.green),
-            flex: 3,
+            child: TextButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Expanded(
+                      flex: 1, child: Container(color: Colors.transparent)),
+                  Text('Appearance'),
+                  Expanded(
+                    flex: 10,
+                    child: Container(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  Text('USD'),
+                  Icon(
+                    Icons.chevron_right,
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
+            ),
           ),
-          Flexible(child: Container(color: Colors.orange)),
+          Flexible(
+            child: TextButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Expanded(
+                      flex: 1, child: Container(color: Colors.transparent)),
+                  Text('Additionally'),
+                  Expanded(
+                    flex: 10,
+                    child: Container(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  Text('USD'),
+                  Icon(
+                    Icons.chevron_right,
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
